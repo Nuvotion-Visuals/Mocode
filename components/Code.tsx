59bd0cb8cb659f9844f46faa7678e8ad6a5219d2
tom-leamon <leamon@outlook.com>
2023-06-23 01:43:07 -0500

    Insert symbols and preserve cursor position

--- components/Code.tsx
+++ components/Code.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { UnControlled as CodeMirror } from 'react-codemirror2';
 import styled from "styled-components";
 
@@ -13,8 +13,11 @@ import 'codemirror/addon/hint/html-hint';
 import 'codemirror/addon/hint/css-hint';
 import 'codemirror/addon/hint/javascript-hint';
 import 'codemirror/addon/hint/show-hint.css';
+import { Symbols } from "./Symbols";
 
 export const Code = ({ type, callback }: { type: string, callback: (val: string) => void }) => {
+  const [editorInstance, setEditorInstance] = useState(null);
+  
   const [currentCode, setCurrentCode] = useState<string>(() => {
     const storedCode = localStorage.getItem(type);
     try {
@@ -38,19 +41,33 @@ export const Code = ({ type, callback }: { type: string, callback: (val: string)
     lineNumbers: true,
     mode: type === 'html' ? 'htmlmixed' : type,
     theme: 'pastel-on-dark',
-    autoCloseTags: true, // Enable auto-closing of HTML tags
+    autoCloseTags: { whenClosing: true, whenOpening: true, indentTags: [] },
     extraKeys: { 'Ctrl-Space': 'autocomplete' }, // Enable code completion with Ctrl+Space
   };
 
+  const insertSymbol = (str: string) => {
+    if (editorInstance) {
+      const doc = editorInstance.getDoc();
+      const cursor = doc.getCursor();
+      doc.replaceRange(str, cursor);
+      const newCursor = { line: cursor.line, ch: cursor.ch + str.length };
+      doc.setCursor(newCursor); // Set the new cursor position
+      editorInstance.focus(); // Focus the editor
+    }
+  }
+  
   return (
     <S.Code>
+      <Symbols insertSymbol={(symbol: string) => insertSymbol(symbol)} />
       {typeof window !== "undefined" && (
         <S.EditorContainer>
           <CodeMirror
-            value={currentCode}
             options={codeMirrorOptions}
             onChange={handleChange}
-          
+            editorDidMount={editor => {
+              setEditorInstance(editor);
+              editor.setValue(currentCode);
+            }}
           />
         </S.EditorContainer>
       )}
