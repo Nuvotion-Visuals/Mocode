1c020bbcc944765ff8d43f4f62f124f17341fd93
tom-leamon <leamon@outlook.com>
2023-06-22 23:19:12 -0500

    HTML closing tags

--- components/Code.tsx
+++ components/Code.tsx
@@ -3,7 +3,6 @@ import dynamic from "next/dynamic";
 import { Symbols } from "../components/Symbols";
 import styled from "styled-components";
 
-// Dynamic import MonacoEditor to avoid SSR issues
 const MonacoEditor = dynamic(import("react-monaco-editor"), { ssr: false });
 
 export const Code = ({ type, callback }: { type: string, callback: (val: string) => void }) => {
@@ -13,7 +12,7 @@ export const Code = ({ type, callback }: { type: string, callback: (val: string)
       return storedCode ? JSON.parse(storedCode) : "";
     } catch (error) {
       console.error("Error parsing stored code:", error);
-      return "<div></div>";
+      return "";
     }
   });
 
@@ -43,6 +42,47 @@ export const Code = ({ type, callback }: { type: string, callback: (val: string)
                   monaco.editor.defineTheme('brilliance-black', data);
                   monaco.editor.setTheme('brilliance-black');
                 })
+
+              monaco.languages.registerCompletionItemProvider('html', 
+                {
+                  triggerCharacters: ['>'],
+                  provideCompletionItems: (model, position) => 
+                  {
+                    const codePre: string = model.getValueInRange({
+                      startLineNumber: position.lineNumber,
+                      startColumn: 1,
+                      endLineNumber: position.lineNumber,
+                      endColumn: position.column,
+                    });
+                
+                    const tag = codePre.match(/.*<(\w+)>$/)?.[1];
+                
+                    if (!tag) {
+                      return;
+                    }
+                    
+                    const word = model.getWordUntilPosition(position);
+                
+                    return {
+                      suggestions: [
+                        {
+                          label: `</${tag}>`,
+                          kind: monaco.languages.CompletionItemKind.EnumMember,
+                          insertText: `$1</${tag}>`,
+                          insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                          range:  {
+                              startLineNumber: position.lineNumber,
+                              endLineNumber: position.lineNumber,
+                              startColumn: word.startColumn,
+                              endColumn: word.endColumn,
+                          },
+                        },
+                      ],
+                    };
+                  },
+                }
+              );
+
               // @ts-ignore
               window.MonacoEnvironment.getWorkerUrl = (
                 _moduleId: string,
